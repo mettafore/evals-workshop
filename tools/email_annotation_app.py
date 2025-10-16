b3454a1dd66d10635326aa30ec30d21cd74e644f
Luv Suneja <luvsuneja@gmail.com>
2025-10-16 21:44:18 +0400

    Changing port to 5001

--- tools/email_annotation_app.py
+++ tools/email_annotation_app.py
@@ -375,4 +375,6 @@ def api_failure_modes_suggest():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5001))
+    app.run(host="0.0.0.0", port=port, debug=True)
